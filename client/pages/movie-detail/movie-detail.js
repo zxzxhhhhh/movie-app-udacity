631abb42afbb644f851d3b6d5637304badc55f6f
xu <zxmaige@163.com>
2018-07-21 18:10:36 +0800

    影片详情页面搭建完毕

--- client/pages/movie-detail/movie-detail.js
+++ client/pages/movie-detail/movie-detail.js
@@ -1,18 +1,56 @@
 // pages/movie-detail/movie-detail.js
+const config = require('../../config.js')
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    movie: {}
   },
+  /**
+   * 随即获取一个电影数据 已知总数为15个
+   */
+  getMovie(movieID) {
+    wx.showLoading({
+      title: '电影数据加载中',
+    })
+    qcloud.request({
+      url: config.service.movieDetail + movieID,
+      success: (result) => {
+        wx.hideLoading()
+        if (!result.data.code) {
+          this.setData({
+            movie: result.data.data
+          })
+        }
+        else {
+          wx.showToast({
+            icon: 'none',
+            title: '加载失败',
+          })
+        }
 
+      },
+      fail: result => {
+        wx.hideLoading()
+        wx.showToast({
+          icon: 'none',
+          title: '加载失败',
+        })
+        console.log('error!' + result);
+      }
+    });
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let movieID = options.id
+    this.getMovie(movieID)
+    console.log('111'+movieID)
   },
 
   /**
