10a97a82f52c05c212570c0e57cc02f5fc2dced3
ZXU <zxmaige@163.com>
2018-07-23 12:56:32 +0800

    电影详情页面：写影评跳转机制实现：showactionsheet

--- client/pages/movie-detail/movie-detail.js
+++ client/pages/movie-detail/movie-detail.js
@@ -9,6 +9,20 @@ Page({
   data: {
     movie: {}
   },
+  showActionSheet(){
+    wx.showActionSheet({
+      itemList: ['文字', '音频'],
+      success: function (res) {
+        console.log("Comment Type is: " + res.tapIndex)
+        wx.navigateTo({
+          url: `../comment-edit/comment-edit?type=${res.tapIndex}`,
+        })
+      },
+      fail: function (res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
   /**
    * 随即获取一个电影数据 已知总数为15个
    */
@@ -51,53 +65,4 @@ Page({
     let movieID = options.id
     this.getMovie(movieID)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
