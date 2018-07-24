e49fe0fcc918236a8ece6a0091a3843d0da70441
ZXU <zxmaige@163.com>
2018-07-24 13:34:45 +0800

    评论列表页面：从服务器数据库获取数据

--- client/pages/comments-list/comments-list.js
+++ client/pages/comments-list/comments-list.js
@@ -1,5 +1,7 @@
 // pages/comments-list/comments-list.js
-
+const config = require('../../config.js')
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js');
+const _ = require('../../utils/util.js')
 const STOP = 0
 const PLAY = 1
 Page({
@@ -8,30 +10,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-    commentList: [
-      {
-        id:12,
-        avatar: '../../images/user.png',
-        username: 'zx1',
-        content:'hhhabababbalalalalbababablallalalababbaballalalla babababalalalbalabalbalanb'
-      },{
-        id:222,
-        avatar: '../../images/user.png',
-        username: 'zx2',
-        content: 'hhhabababbalalalalbababablallalalababbaballalalla babababalalalbalabalbalanb',
-        voices:'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
-      }
-    ],
+    commentList: [],
     playStatus: STOP,
     audioLength:15
   },
+  //获取评论详情
+  getCommentList(id) {
+    wx.showLoading({
+      title: '评论数据加载中',
+    })
+    qcloud.request({
+      url: config.service.comment,
+      login: true,
+      method: 'GET',
+      data: {
+        movie_id: id
+      },
+      success: (result) => {
+        wx.hideLoading()
+        console.log(result)
+        if (!result.data.code) {
 
+          this.setData({
+            commentList: result.data.data.map(item => {
+              let itemDate = new Date(item.create_time)
+              item.createTime = _.formatTime(itemDate)
+              return item
+            })
+          })
+        }
+        else {
+          wx.showToast({
+            title: '加载失败',
+          })
+        }
+
+      },
+      fail: result => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '加载失败',
+        })
+        console.log('error!');
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     let movieID = options.id
-    console.log(movieID)
+    this.getCommentList(movieID)
+    //
     // 这里为什么放到函数onTapPlay中是不行的（if中可以调用，else中不行）
     this.innerAudioContext = wx.createInnerAudioContext()
   },
@@ -48,13 +78,14 @@ Page({
         comment = commentList[i]
         break
       }
-
-      
-      
     }
     
     if (comment.voices){
       this.innerAudioContext.src = comment.voices
+      console.log(this.innerAudioContext.duration)
+      this.setData({
+        audioLength: this.innerAudioContext.duration
+      })
 
       if (this.data.playStatus == 0)//没有播放
       {
@@ -88,7 +119,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log("on show")
   },
 
   /**
