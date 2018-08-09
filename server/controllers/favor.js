dd633cb6b9209737fec2f2d14a43c2348b304c55
ZXU <zxmaige@163.com>
2018-08-09 19:38:48 +0800

    修改用户页面的服务器代码，使显示音频长度

--- server/controllers/favor.js
+++ server/controllers/favor.js
@@ -16,9 +16,9 @@ module.exports = {
   list: async ctx => {
     let user = ctx.state.$wxInfo.userinfo.openId
 
-    let favorList = await DB.query('SELECT  comment_movie.id AS `comment_id`, movies.image AS `image`, movies.title AS `title` ,  comment_movie.username AS `username`,  comment_movie.avatar AS `avatar`, comment_movie.content AS `content`, comment_movie.voices AS `voices` FROM  movies LEFT JOIN comment_movie ON movies.id = comment_movie.movie_id LEFT JOIN favorites ON favorites.comment_id = comment_movie.id WHERE favorites.user = ? ORDER BY favorites.id', [user])
+    let favorList = await DB.query('SELECT  comment_movie.id AS `comment_id`, movies.image AS `image`, movies.title AS `title` ,  comment_movie.username AS `username`,  comment_movie.avatar AS `avatar`, comment_movie.content AS `content`, comment_movie.voices AS `voices`, comment_movie.voice_length AS `voice_length` FROM  movies LEFT JOIN comment_movie ON movies.id = comment_movie.movie_id LEFT JOIN favorites ON favorites.comment_id = comment_movie.id WHERE favorites.user = ? ORDER BY favorites.id', [user])
 
-    let myCommentList = await DB.query('SELECT  comment_movie.id AS `comment_id`, movies.image AS `image`, movies.title AS `title`, comment_movie.content AS `content`, comment_movie.voices AS `voices` FROM  movies LEFT JOIN comment_movie ON movies.id = comment_movie.movie_id  WHERE comment_movie.user = ? ORDER BY comment_movie.id', [user])
+    let myCommentList = await DB.query('SELECT  comment_movie.id AS `comment_id`, movies.image AS `image`, movies.title AS `title`, comment_movie.content AS `content`, comment_movie.voices AS `voices` , comment_movie.voice_length AS `voice_length` FROM  movies LEFT JOIN comment_movie ON movies.id = comment_movie.movie_id  WHERE comment_movie.user = ? ORDER BY comment_movie.id', [user])
 
     ctx.state.data = { favorList, myCommentList}
   }
